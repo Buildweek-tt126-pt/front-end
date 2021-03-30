2f83031b618c2d319067adaf3a2fd23a9f143d6b
Barry Porch <69640436+bporch84@users.noreply.github.com>
2021-03-29 21:13:30 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from "react"
 import { Route, Switch } from "react-router-dom"
+import styled from "styled-components"
 
 import RecipesList from "./components/RecipesList"
 import MainBody from "./components/MainBody"
 // import PrivateRoute from "./components/PrivateRoute"
 import AddRecipe from "./components/AddRecipe"
 
+const StyledApp = styled.div`
+background-color: #f5f5f5;
+overflow: hidden;
+`
+
 function App() {
   return (
-    <div>      
+    <StyledApp>      
       <Switch>
         <Route path="/recipes" component={RecipesList} />
         <Route path="/add-recipe" component={AddRecipe} />
         <Route component={MainBody} />
       </Switch>
-    </div>
+
+    </StyledApp>
   );
 }
 
